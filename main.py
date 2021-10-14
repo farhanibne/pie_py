4ab51058de1b4a735e181007f474e16368ec74f3
farhan ibne saif <84982846+farhanibne@users.noreply.github.com>
2021-10-14 10:07:18 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as pyplot
+import numpy as np
+
+data = np.array([40,20,10,25,5])
+labels = ['python','android','java','javascript','go']
+
+pyplot.pie(data,labels = labels,shadow=True)
+pyplot.show()
